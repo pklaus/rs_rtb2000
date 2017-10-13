a98cfd66946ea75a3776ef0a232dcc2858a05c85
Philipp Klaus <philipp.klaus@gmail.com>
2017-10-13 17:50:48 +0200

    generalize the file interpretation: headers of 9 bytes

--- analyze_settings_file.py
+++ analyze_settings_file.py
@@ -17,22 +17,20 @@ def analyze_settings_file(path="./settings.set"):
     dirname, filename = os.path.split(path)
     with open(path, 'rb') as f:
         data = f.read()
-    pos = data.find(b'BM')
-    size = struct.unpack('<L', data[pos+2:pos+6])[0]
-    img_filename = os.path.splitext(filename)[0] + '.bmp'
-    assert img_filename != filename
-    with open(os.path.join(dirname, img_filename), 'wb') as f:
-        f.write(data[pos:pos+size])
-    pos += size
-    size = struct.unpack('>H', data[pos+3:pos+5])[0]
-    pos += 9 # header length between bmp and ASCII SCPI setup cmds
-    assert (pos+size) == len(data)
-    cmds = data[pos:pos+size]
-    cmds = cmds.decode('ascii')
-    cmds_filename = os.path.splitext(filename)[0] + '.cmds'
-    assert cmds_filename != filename
-    with open(os.path.join(dirname, cmds_filename), 'w') as f:
-        f.write(cmds)
+    cntr = 0
+    pos = 0x19
+    while pos < len(data):
+        size = struct.unpack('>L', data[pos+1:pos+5])[0]
+        print("#: {:2d}  Offset: 0x{:04X}  Type: 0x{:02X}  Size: 0x{:04X}".format(cntr, pos, data[pos], size))
+        chunk = data[pos+9:pos+9+size]
+        new_filename = os.path.splitext(filename)[0] + '.' + str(cntr) + '_' + hex(data[pos]) + '.other'
+        assert new_filename != filename
+        with open(os.path.join(dirname, new_filename), 'wb') as f:
+            f.write(chunk)
+        pos += 9 + size
+        cntr += 1
+    if pos != len(data):
+        print("Not aligned?")
 
 if __name__ == "__main__":
     args = parse_args()
