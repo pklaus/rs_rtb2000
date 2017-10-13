87350867f8f9bfbbcc025be7ecc1cbc193df3520
Philipp Klaus <philipp.klaus@gmail.com>
2017-10-13 16:58:43 +0200

    set screenshot name via --filename; default: current date&time

--- get_screenshot.py
+++ get_screenshot.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
-from helpers import get_instrument, write, query, ieee_488_2_block_data
+from helpers import parse_args, get_instrument, write, query, ieee_488_2_block_data
+from datetime import datetime as dt
 
-def get_screenshot(device=None, backend=None):
+def get_screenshot(filename=None, device=None, backend=None):
     inst = get_instrument(device, backend=backend)
+    if not filename: filename = dt.now().replace(microsecond=0).isoformat('_').replace(':','-') + '.png'
     if query(inst, "ACQuire:STATe?") not in ("STOP", "BRE"):
         write(inst, "STOP")
     if query(inst, "HCOPy:LANGuage?") != "PNG":
         write(inst, "HCOPy:LANGuage PNG")
     data = inst.query_raw("HCOPy:DATA?")
     data = ieee_488_2_block_data(data)
-    with open('tmp.png', 'wb') as f:
+    with open(filename, 'wb') as f:
         f.write(data)
 
 if __name__ == "__main__":
-    get_screenshot()
+    def add_more_args(parser):
+        parser.add_argument('--filename')
+    args = parse_args(add_more_args)
+    get_screenshot(args.filename, args.device, args.backend)
